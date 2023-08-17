032ee8162aa7bb0feb5dce12012e6e1a64109f29
OrlandoRios90 <orlyr21@gmail.com>
2023-08-17 15:41:50 -0400

    finished the basic gameplay, intend to add more features later

--- candy.js
+++ candy.js
@@ -13,6 +13,8 @@ window.onload = () => {
 
     window.setInterval(function() {
         crushCandy();
+        slideCandyDown();
+        generateCandy();
     }, 100);
 }
 
@@ -71,6 +73,11 @@ function dragDrop() {
 
 function dragEnd() {
 
+    //if either of the swapped tiles is blank then exit the fxn and no swapping occurs
+    if (currTile.src.includes("blank") || targetTile.src.includes("blank")) {
+        return; 
+    }
+
     //split the id at the dash, return an array of strings
     let currCoords = currTile.id.split("-"); 
     let currRow = parseInt(currCoords[0]);
@@ -87,19 +94,26 @@ function dragEnd() {
 
     let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
 
-    
-
     if (isAdjacent) {
         let currImg = currTile.src;
         let targetImg = targetTile.src;
         currTile.src = targetImg;
         targetTile.src = currImg;
+
+        let validMove = checkIfValid();
+        if (!validMove) {
+            let currImg = currTile.src;
+            let targetImg = targetTile.src;
+            currTile.src = targetImg;
+            targetTile.src = currImg;
+        }
     }
 }
 
 function crushCandy () {
     //crushFive(), crushFour, etc...
     crushThree();
+    document.getElementById("score").innerText = score;
 }
 
 //function that actually "crushes" the candy and replaces it with blank tile
@@ -108,6 +122,13 @@ function replaceWithBlank(candy1,candy2,candy3) {
         candy1.src = './images/blank.png';
         candy2.src = './images/blank.png';
         candy3.src = './images/blank.png';
+        score += 30;
+    }
+}
+
+function areAllThreeSame(candy1,candy2,candy3) {
+    if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+        return true;
     }
 }
 
@@ -130,6 +151,52 @@ function crushThree () {
             let candy3 = board[r+2][c];
             replaceWithBlank(candy1,candy2,candy3);
         }
+    }
+}
+
+function checkIfValid () {
+    for (let r = 0; r < rows; r ++){
+        for (let c = 0; c< cols -2 ; c++) { //cols - 2 to check 2 candies ahead for 3 in a row
+            let candy1 = board[r][c];
+            let candy2 = board[r][c+1];
+            let candy3 = board[r][c+2];
+            if (areAllThreeSame(candy1,candy2,candy3)) return true;
+        }
         
+    }
+
+    for (let c = 0; c < cols; c++) {
+        for (let r = 0; r < rows - 2; r++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r+1][c];
+            let candy3 = board[r+2][c];
+            if (areAllThreeSame(candy1,candy2,candy3)) return true;
+        }
+    }
+
+    return false;
+}
+
+function slideCandyDown() {
+    for(let c = 0; c < cols; c++) {
+        let ind = rows - 1;
+        for (let r = cols - 1; r >= 0; r--) {
+            if (!board[r][c].src.includes("blank")) {
+                board[ind][c].src = board[r][c].src; 
+                ind -= 1;
+            }
+        }
+
+        for (let r = ind; r >= 0; r--) {
+            board[r][c].src = './images/blank.png';
+        }
+    }
+}
+
+function generateCandy() {
+    for(let c = 0; c < cols; c++) {
+        if (board[0][c].src.includes("blank")) {
+            board[0][c].src = './images/' + randomCandy() + '.png';
+        }
     }
 }
